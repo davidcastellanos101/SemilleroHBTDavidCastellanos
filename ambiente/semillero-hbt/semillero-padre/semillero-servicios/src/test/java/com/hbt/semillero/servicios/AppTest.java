28f33e6bf7473c13581348b02d05692445c8cd88
David Castellanos <davidcastellanos101@gmail.com>
2019-10-30 09:26:00 -0500

    Tarea sesión 2

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
@@ -1,14 +1,33 @@
 package com.hbt.semillero.servicios;
 
+import java.util.ArrayList;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.hbt.semillero.entidades.EstadoEnum;
+/**
+ * 
+ * <b>Descripción:<b> Clase encargada de ejecutar las pruebas unitarias
+ * <b>Caso de Uso:<b> 
+ * @author David Castellanos
+ * @version
+ */
 public class AppTest {
 
 	public void shouldAnswerWithTrue() {
 
 	}
 
+	/**
+	 * 
+	 * Metodo encargado de invertir una cadena
+	 * <b>Caso de Uso</b>
+	 * @author David Castellanos
+	 * 
+	 * @param cadena cadena a invertir
+	 * @return cadena invertida
+	 */
 	private String invertirCadena(String cadena) {
 
 		String cadenaInvertida = "";
@@ -23,6 +42,13 @@ public class AppTest {
 
 	}
 
+	/**
+	 * 
+	 * Metodo encargado de 
+	 * <b>Caso de Uso</b>
+	 * @author David Castellanos
+	 *
+	 */
 	@Test
 	public void primeraPU() {
 		Long a = 100L;
@@ -33,6 +59,13 @@ public class AppTest {
 		Assert.assertEquals(resultado, resultadoEsperado);
 	}
 
+	/**
+	 * 
+	 * Metodo encargado de probar el método invertirCadena(String cadena)
+	 * <b>Caso de Uso</b>
+	 * @author David Castellanos
+	 *
+	 */
 	@Test
 	public void segundaPU() {
 		String resultado = invertirCadena("");
@@ -61,7 +94,7 @@ public class AppTest {
 
 		resultado = invertirCadena("");
 		Assert.assertTrue(resultado.length() == 0,
-				"Se esperaban 0 caracteres pero el método retornó " + resultado.length());
+				"Se esperaban 0 caracteres pero el método retorno " + resultado.length());
 
 		resultado = invertirCadena("      ");
 		Assert.assertTrue(resultado.length() == 6,
@@ -89,6 +122,46 @@ public class AppTest {
 		resultadoEsperado = "a";
 		Assert.assertNotEquals(resultadoEsperado, resultado,
 				"Se esperaba '" + resultadoEsperado + "' pero el método retornó '" + resultado + "'");
+
+	}
+
+	/**
+	 * 
+	 * Metodo encargado de probar el Enum EstadoEnum <b>Caso de Uso</b>
+	 * 
+	 * @author David Castellanos
+	 *
+	 */
+	@Test
+	public void terceraPU() {
+		// Instanciar un enum de la clase EstadoEnum
+		EstadoEnum estadoEnum = EstadoEnum.ACTIVO;
+		// Devolver un String con el nombre de la constante (ACTIVO)
+		String resultado1 = estadoEnum.name();
+		String resultadoEsperado1 = "ACTIVO";
+		Assert.assertEquals(resultadoEsperado1, resultado1,
+				"Se esperaba '" + resultadoEsperado1 + "' pero el método retornó '" + resultado1 + "'");
+
+		estadoEnum = EstadoEnum.INACTIVO;
+		// Devolver un String con el nombre de la constante (INACTIVO)
+		resultado1 = estadoEnum.name();
+		resultadoEsperado1 = "INACTIVO";
+		Assert.assertEquals(resultadoEsperado1, resultado1,
+				"Se esperaba '" + resultadoEsperado1 + "' pero el método retornó '" + resultado1 + "'");
+
+		// Devolver un entero con la posición del enum según está declarada
+		Integer resultado2 = estadoEnum.ordinal();
+		Integer resultadoEsperado2 = 1;
+		// Comparar el enum con el parámetro según el orden en el que están declarados
+		// lo enum
+		Assert.assertEquals(resultadoEsperado2, resultado2,
+				"Se esperaba '" + resultadoEsperado2 + "' pero el método retornó '" + resultado2 + "'");
+
+		// Devolver un array que contiene todos los enum
+		EstadoEnum[] resultado3 = estadoEnum.values();
+		EstadoEnum[] resultadoEsperado3 = { EstadoEnum.ACTIVO, EstadoEnum.INACTIVO };
+		Assert.assertEquals(resultadoEsperado3, resultado3,
+				"Se esperaba '" + resultadoEsperado3 + "' pero el método retornó '" + resultado3 + "'");
 
 	}
 }
