10cd888db69804865c7b9c0845804712f31e11e8
David Castellanos <davidcastellanos101@gmail.com>
2019-11-13 18:50:19 -0500

    maratón progra

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/EjeciciosTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/EjeciciosTest.java
@@ -3,6 +3,8 @@
  */
 package com.hbt.semillero.servicios;
 
+import org.testng.annotations.Test;
+
 /**
  * <b>Descripción:<b> Clase que determina
  * <b>Caso de Uso:<b> 
@@ -10,5 +12,22 @@ package com.hbt.semillero.servicios;
  * @version 
  */
 public class EjeciciosTest {
+	
+	@Test(enabled = true)
+	public void ejercicio1() {
+		//este es un identificador válido ya que empieza por una letra y no tiene palabras reservadas
+		String A$B;
+		//este es un identificador válido ya que empieza por una letra y no tiene palabras reservadas
+		String _helloWorld; 
+		//este no es un identificador valido ya que true es una palabra reservada
+		String true;
+		//este no es un identificador valido ya que java es una palabra reservada
+		String java.lang;
+		//este es un identificador válido ya que empieza por una letra y no tiene palabras reservadas
+		String pPublic;
+		//este no es un identificador valido ya que empieza por un numero, loc ual no es permitido
+		String 1980S;
+		
+	}
 
 }
