a9f03350f274d054e24aeedd90ac9fd6475c973a
David Castellanos <davidcastellanos101@gmail.com>
2019-11-13 19:16:17 -0500

    maratón progra

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/heinsohn/semillero/pojo/EjerciciosPOJO.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/heinsohn/semillero/pojo/EjerciciosPOJO.java
@@ -10,5 +10,40 @@ package com.heinsohn.semillero.pojo;
  * @version 
  */
 public class EjerciciosPOJO {
+	
+	
+	
+	/**
+	 * Constructor de la clase.
+	 */
+	public EjerciciosPOJO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 * Metodo encargado de probar si un numero es primo
+	 * <b>Caso de Uso</b>
+	 * @author Dave
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public boolean esPrimo(int n) {
+		int contador = 0;
+
+		for (int i = 1; i <= n; i++) {
+			int residuo = n % i;
+			if (residuo == 0) {
+				contador++;
+			}
+		}
+
+		if (contador <= 2) {
+			return true;
+		}
+		return false;
+	}
 
 }
--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/EjeciciosTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/EjeciciosTest.java
@@ -6,6 +6,8 @@ package com.hbt.semillero.servicios;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.heinsohn.semillero.pojo.EjerciciosPOJO;
+
 /**
  * <b>Descripción:<b> Clase que determina <b>Caso de Uso:<b>
  * 
@@ -64,29 +66,8 @@ public class EjeciciosTest {
 		// Empty = true, Brand = empty
 	}
 
-	/**
-	 * 
-	 * Metodo encargado de probar si un numero es primo
-	 * <b>Caso de Uso</b>
-	 * @author Dave
-	 * 
-	 * @param n
-	 * @return
-	 */
-	public boolean esPrimo(int n) {
-		int contador = 0;
 
-		for (int i = 1; i <= n; i++) {
-			if ((n % i) == 0) {
-				contador++;
-			}
-		}
 
-		if (contador <= 2) {
-			return true;
-		}
-		return false;
-	}
 
 	/**
 	 * 
@@ -97,12 +78,13 @@ public class EjeciciosTest {
 	 */
 	@Test(enabled = true)
 	public void ejercicio3() {
+		EjerciciosPOJO ejercicios = new EjerciciosPOJO();
 		int n1 = 5;
 		int n2 = 222;
 		int n3 = 0;
-		Assert.assertTrue(esPrimo(n1)); //Esta pruba pasara exitosamente
-		Assert.assertTrue(esPrimo(n2)); //Esta prueba no pasará, 222 no es primo
-		Assert.assertTrue(esPrimo(n3)); //Esta pruba pasara exitosamente
+		Assert.assertTrue(ejercicios.esPrimo(n1)); //Esta pruba pasara exitosamente
+		Assert.assertTrue(ejercicios.esPrimo(n2)); //Esta prueba no pasará, 222 no es primo
+		Assert.assertTrue(ejercicios.esPrimo(n3)); //Esta pruba pasara exitosamente
 	}
 
 }
